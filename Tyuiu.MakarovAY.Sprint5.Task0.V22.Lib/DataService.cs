f330a089f28f18adfb0e068a17ac6b007d4cc799
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-11-26 23:15:07 +0500

    Add project files.

---
+++ Tyuiu.MakarovAY.Sprint5.Task0.V22.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.MakarovAY.Sprint5.Task0.V22.Lib
+{
+    public class DataService : ISprint5Task0V22
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string rn = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double z = (Math.Pow((1.0 - (double)x), 2) / (-3.0 * (double)x));
+            z = Math.Round(z, 2);
+            File.WriteAllText(rn, z.ToString());
+            return rn;
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint5.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MakarovAY.Sprint5.Task0.V22.Lib;
+namespace Tyuiu.MakarovAY.Sprint5.Task0.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+            string path = ds.SaveToFileTextData(3); // Получаем реальный путь из Temp папки
+
+            FileInfo fileInfo = new FileInfo(path); // Используем этот путь
+            bool fileExistst = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExistst);
+
+        }
+    }
+}
