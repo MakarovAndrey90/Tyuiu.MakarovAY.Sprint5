8a60e0e1f620022c8a23a7d2e0a6b808188c3c80
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-11-28 00:24:01 +0500

    Task5

---
+++ Tyuiu.MakarovAY.Sprint5.Task5.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MakarovAY.Sprint5.Task5.V23.Lib;
+namespace Tyuiu.MakarovAY.Sprint5.Task5.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\user\source\repos\Tyuiu.BurdovKS.Sprint5\Tyuiu.MakarovAY.Sprint5.Task2.V16\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExistst = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExistst);
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint5.Task5.V23/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MakarovAY.Sprint5.Task5.V23.Lib;
+namespace Tyuiu.MakarovAY.Sprint5.Task5.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Макаров А. Я. | РППБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Макаров А. Я. | РППБ-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:DataSprint5InPutDataFileTask5V23.txt (файл взять из архива   *");
+            Console.WriteLine("* согласно вашему варианту.                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask5V23.txt";
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Данные находятся в файле: " + path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+
+}
