32465c1d3be147cff3bab41ab6ebf8fed4e1a429
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-11-27 22:59:21 +0500

    Task3

---
+++ Tyuiu.MakarovAY.Sprint5.Task3.V23.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.IO;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.MakarovAY.Sprint5.Task3.V23.Lib
+{
+    public class DataService : ISprint5Task3V23
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.GetTempFileName();
+            double z = (1 + Math.Pow((double)x, 3)) / Math.Pow((double)x, 2);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(z));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint5.Task3.V23/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.MakarovAY.Sprint5.Task3.V23.Lib;
+namespace Tyuiu.MakarovAY.Sprint5.Task3.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            Console.Title = "Спринт #5 | Выполнил: Макаров А. Я. | РППБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс file                                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Макаров А. Я. | РППБ-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить    *");
+            Console.WriteLine("* в бинарный файл OutPutFileTask0.txt и вывести на консоль.              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Файл" + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+
+}
