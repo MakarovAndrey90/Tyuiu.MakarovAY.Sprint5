e29a70fcbee4951098c62d0097a1759d71ba9e70
Андрей Макаров <tyuiu.MakarovAY@outlook.com>
2025-11-27 23:50:32 +0500

    Task4

---
+++ Tyuiu.MakarovAY.Sprint5.Task4.V5.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Xml.Linq;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.MakarovAY.Sprint5.Task4.V5.Lib
+{
+    public class DataService : ISprint5Task4V5
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string r = File.ReadAllText(path);
+            double x = double.Parse(r, CultureInfo.InvariantCulture);
+            double res = Math.Round((4.26 * x) / Math.Sin(x), 2);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MakarovAY.Sprint5.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MakarovAY.Sprint5.Task4.V5.Lib;
+namespace Tyuiu.MakarovAY.Sprint5.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\user\source\repos\Tyuiu.BurdovKS.Sprint5\Tyuiu.MakarovAY.Sprint5.Task2.V16\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExistst = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExistst);
+        }
+    }
